983bed19569f3595d964094df5359ec8bdee14ce
Victor Berchet <victor@suumit.com>
2012-01-13 16:56:17 +0100

    List supported wurst types

--- Command/WurstCommand.php
+++ Command/WurstCommand.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * WurstCommand.
@@ -16,13 +18,33 @@ class WurstCommand extends ContainerAwareCommand
 {
     const ERROR_WURST_NOT_FOUND = 1;
 
+    protected $wurstTypes = array();
+
+    public function __construct($name = null)
+    {
+        $finder = Finder::create()
+            ->in(__DIR__.'/../Resources/wurst')
+            ->name('*.txt')
+            ->depth(0)
+            ->filter(function (SplFileInfo $file) {
+                return $file->isReadable();
+            })
+        ;
+
+        foreach ($finder as $file) {
+            $this->wurstTypes[] = basename($file->getRelativePathName(), '.txt');
+        }
+
+        parent::__construct($name);
+    }
+    
     protected function configure()
     {
         $this
             ->setName('wurst:print')
             ->addOption('mit-pommes', null, InputOption::VALUE_NONE, 'Mit Pommes?')
             ->addOption('mit-mayo', null, InputOption::VALUE_NONE, 'Mit Mayo?')
-            ->addArgument('type', null, 'Which type of würst you want?', 'classic')
+            ->addArgument('type', null, sprintf('Which type of würst you want (%s)?', implode(', ', $this->wurstTypes)), 'classic')
             ->setHelp('Please ask your local curry würst retailer.')
         ;
     }
@@ -31,8 +53,12 @@ class WurstCommand extends ContainerAwareCommand
     {
         $wurstFile = sprintf(__DIR__.'/../Resources/wurst/%s.txt', $input->getArgument('type'));
         if (!is_readable($wurstFile)) {
-            $output->writeln('<error>This würst is not part of this bundle. Try "classic" or consider contributing!</error>');
-
+            $output->writeln(
+            $output->writeln(sprintf(
+                '<error>This würst is not part of this bundle. Try one of "%s" or consider contributing!</error>',
+                implode(', ', $this->wurstTypes)
+            ));
+            
             return self::ERROR_WURST_NOT_FOUND;
         }
 
