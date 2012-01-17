9178a98e5ea1bf6bb67ba79c0a012bc08365493c
gnugat <loic.chardonnet@gmail.com>
2012-01-17 11:29:33 +0000

    [test] Creating types test

--- Tests/Command/WurstCommandTest.php
+++ Tests/Command/WurstCommandTest.php
@@ -5,22 +5,82 @@ namespace MarcW\Bundle\WurstBundle\Tests\Command;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use MarcW\Bundle\WurstBundle\Command\WurstCommand;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class WurstCommandTest extends \PHPUnit_Framework_TestCase
-{  
-    public function testDefaultCommand()
+{
+    private $wurstResourcesDirectory;
+    private $command;
+    private $commandTester;
+    private $wurstTypes;
+    
+    public function setUp()
+    {
+        $this->findWurstResourcesDirectory();
+        
+        $this->findCommand();
+        $this->commandTester = new CommandTester($this->command);
+        
+        $this->findWurstTypes();
+    }
+    
+    private function findWurstResourcesDirectory()
+    {
+        $sourceDirectory = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR;
+        $resourceDirectory = $sourceDirectory.'Resources'.DIRECTORY_SEPARATOR;
+        
+        $this->wurstResourcesDirectory = $resourceDirectory.'wurst'.DIRECTORY_SEPARATOR;
+    }
+    
+    private function findCommand()
     {
         $mockedKernel = $this->getMock('Symfony\\Component\\HttpKernel\\Kernel', array(), array(), '', false);
         $application = new Application($mockedKernel);
         $application->add(new WurstCommand());
 
-        $command = $application->find('wurst:print');
-        $commandTester = new CommandTester($command);
-        $commandTester->execute(array('command' => $command->getName()));
-        
-        $expectedOutput = file_get_contents(__DIR__.'/../../Resources/wurst/classic.txt');
+        $this->command = $application->find('wurst:print');
+    }
+    
+    private function findWurstTypes()
+    {
+        $foundFiles = Finder::create()
+            ->in($this->wurstResourcesDirectory)
+            ->name('*.txt')
+            ->depth(0)
+            ->filter(function (SplFileInfo $file) {
+                return $file->isReadable();
+            })
+        ;
+
+        foreach ($foundFiles as $foundFile) {
+            $this->wurstTypes[] = basename($foundFile->getRelativePathName(), '.txt');
+        }
+    }
+
+    public function testDefaultCommand()
+    {
+        $this->commandTester->execute(array('command' => $this->command->getName()));
+
+        $expectedOutput = file_get_contents($this->wurstResourcesDirectory.'classic.txt');
         $expectedOutput .= PHP_EOL;
 
-        $this->assertEquals($commandTester->getDisplay(), $expectedOutput);
+        $this->assertSame($expectedOutput, $this->commandTester->getDisplay());
+    }
+
+    public function testTypes()
+    {
+        foreach ($this->wurstTypes as $wurstType)
+        {
+            $this->commandTester->execute(array(
+                'command' => $this->command->getName(),
+                'type' => $wurstType
+            ));
+
+            $expectedOutput = file_get_contents($this->wurstResourcesDirectory.$wurstType.'.txt');
+            $expectedOutput .= PHP_EOL;
+
+            $this->assertSame($expectedOutput, $this->commandTester->getDisplay());
+        }
     }
 }
